6ea526c8ba4c09e6b745b4833a98d54e3ca6f74c
willsb93 <williamrabuske93@gmail.com>
2020-06-30 14:40:00 -0400

    functions ok, starting css

--- src/App.vue
+++ src/App.vue
@@ -6,9 +6,20 @@
     <main>
         <section class="player">
             <h2 class = "song-title">{{currentSong.title}} - <span>{{currentSong.artist}}</span></h2>
-        </section>
+            <div class="controls">
+              <button class = "prev" @click="prev">Prev</button>
+              <button class = "play" v-if = !isPlaying @click="play">Play</button>
+              <button class = "pause" v-else @click="pause">Pause</button>
+              <button class = "next" @click="next">Next</button>
+            </div>
+          </section>
+          <section class = "playlist">
+            <h4>Sample Playlist</h4>
+            <button v-for = "song in songs" :key= "song.src" @click = play(song) :class = "(song.src == currentSong.src) ? 'song playing' : 'song'">
+              {{song.title}} - {{song.artist}}</button>
+          </section>
+          <div><a href = "https://www.bensound.com/" target="_blank"> Royalty Free Music from Bensound </a></div>
     </main>
-  
   </div>
 </template>
 
@@ -18,10 +29,9 @@ export default {
   name: 'app',
   data () {
     return {
-      currentSong: {
-        title: "Song Title"
-      },
+      currentSong: {},
       index: 0,
+      isPlaying: false,
       songs: [
         {
           title: "Dance",
@@ -47,13 +57,77 @@ export default {
       player: new Audio()
     }
   },
+  methods: {
+    play(song){
+        if (typeof song.src != "undefined") {
+          this.current = song;
+          this.player.src = this.current.src;
+        }
+        this.player.play();
+        this.player.addEventListener ("ended", function (){
+          this.index++;
+          if (this.index > this.songs.length - 1) {
+            this.index = 0;
+          }
+
+          this.currentSong = this.songs[this.index];
+          this.play(this.current);
+        }.bind(this));
+        this.isPlaying = true;
+    },
+   pause () {
+        this.player.pause();
+        this.isPlaying = false;
+   },
+   next (){
+        this.index++;
+        if (this.index > this.songs.length - 1) {
+          this.index = 0;
+        }
+        this.currentSong = this.songs[this.index];
+        this.play(this.currentSong);
+   },
+   prev (){
+        this.index--;
+        if (this.index < 0) {
+          this.index = this.songs.length - 1;
+        }
+        this.currentSong = this.songs[this.index];
+        this.play(this.currentSong);
+   }
+  },
   created () {
     this.currentSong = this.songs[this.index];
-    this.player.src = this.current.src;
+    this.player.src = this.currentSong.src;
+    
   }
 }
 </script>
 
 <style>
+
+* {
+  margin: 0;
+  padding : 0;
+  box-sizing: border-box;
+}
+
+body {
+  font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+}
+
+header {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 15px;
+}
+
+
+
+
+
+
+
 
 </style>
