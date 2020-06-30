a19b20d5310f61dc7bdd2abfeaf8f102c0e8eab0
willsb93 <williamrabuske93@gmail.com>
2020-06-30 13:49:40 -0400

    player created

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,56 @@
 <template>
   <div id="app">
+    <header>
+      <h1>My music player</h1>
+    </header>
+    <main>
+        <section class="player">
+            <h2 class = "song-title">{{currentSong.title}} - <span>{{currentSong.artist}}</span></h2>
+        </section>
+    </main>
+  
   </div>
 </template>
 
 <script>
 
 export default {
-  name: 'App',
+  name: 'app',
+  data () {
+    return {
+      currentSong: {
+        title: "Song Title"
+      },
+      index: 0,
+      songs: [
+        {
+          title: "Dance",
+          artist: "bensound",
+          src: require ("./assets/bensound-dance.mp3")
+        },
+        {
+          title: "Dreams",
+          artist: "bensound",
+          src: require ("./assets/bensound-dreams.mp3")
+        },
+        {
+          title: "Dubstep",
+          artist: "bensound",
+          src: require ("./assets/bensound-dubstep.mp3")
+        },
+        {
+          title: "Endlessmotion",
+          artist: "bensound",
+          src: require ("./assets/bensound-endlessmotion.mp3")
+        }
+      ],
+      player: new Audio()
+    }
+  },
+  created () {
+    this.currentSong = this.songs[this.index];
+    this.player.src = this.current.src;
+  }
 }
 </script>
 
